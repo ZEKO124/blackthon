41faa2b28dd345b91205edd25f9ff77ac987e3e5
ZEKO <104495224+ZEKO124@users.noreply.github.com>
2023-03-25 07:43:21 +0300

    Update pluginmanager.py

--- jmub/utils/pluginmanager.py
+++ jmub/utils/pluginmanager.py
@@ -13,7 +13,7 @@ from ..core.session import jmub
 from ..helpers.utils import _format, _jmthonutils, install_pip, reply_id
 from .decorators import admin_cmd, sudo_cmd
 
-LOGS = logging.getLogger("سورس جبثون")
+LOGS = logging.getLogger("سورس بلاكثون")
 
 
 def load_module(shortname, plugin_path=None):
