cd0b0cb803220347cc5c2a43e3cff387e5b0021c
ZEKO <104495224+ZEKO124@users.noreply.github.com>
2023-03-29 02:11:49 +0300

    Update session.py

--- jmub/core/session.py
+++ jmub/core/session.py
@@ -15,7 +15,7 @@ loop = None
 if Config.STRING_SESSION:
     session = bothseesion(Config.STRING_SESSION, LOGS)
 else:
-    session = "Blackthon"
+    session = "JMTHON"
 
 try:
     jmub = JmthonClient(
@@ -35,7 +35,7 @@ except Exception as e:
     sys.exit()
 
 jmub.tgbot = tgbot = JmthonClient(
-    session="BlackthonTgbot",
+    session="JMTHONTgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     loop=loop,
