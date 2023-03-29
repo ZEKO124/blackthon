c8b693c5dfecfdd47bd5d5b3cb8962afc47f4241
ZEKO <104495224+ZEKO124@users.noreply.github.com>
2023-03-29 03:43:45 +0300

    Update session.py

--- jmub/core/session.py
+++ jmub/core/session.py
@@ -1,42 +1,45 @@
 import sys
 
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
-from telethon.sessions import StringSession
 
 from ..Config import Config
-from .client import CatUserBotClient
+from .bothseesion import bothseesion
+from .client import JmthonClient
+from .logger import logging
 
-version = "2.10.6"
+LOGS = logging.getLogger("سورس بلاكثون")
+__version__ = "2.10.6"
 
 loop = None
 
 if Config.STRING_SESSION:
-    session = StringSession(str(Config.STRING_SESSION))
+    session = bothseesion(Config.STRING_SESSION, LOGS)
 else:
-    session = "catuserbot"
+    session = "jmthon"
 
 try:
-    catub = CatUserBotClient(
+    jmub = JmthonClient(
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         loop=loop,
-        app_version=version,
+        app_version=__version__,
         connection=ConnectionTcpAbridged,
         auto_reconnect=True,
         connection_retries=None,
     )
 except Exception as e:
-    print(f"STRING_SESSION - {e}")
+    print(
+        f"STRING_SESSION WRONG PLZ MAKE A NEW SESSION - {e}\n كود سيشن تيليثون الذي وضعته غير صالح"
+    )
     sys.exit()
 
-
-catub.tgbot = tgbot = CatUserBotClient(
-    session="CatTgbot",
+jmub.tgbot = tgbot = JmthonClient(
+    session="jmthonTgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     loop=loop,
-    app_version=version,
+    app_version=__version__,
     connection=ConnectionTcpAbridged,
     auto_reconnect=True,
     connection_retries=None,
