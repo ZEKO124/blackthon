f195d6a95ca863c803125521d597788a158cbef3
ZEKO <104495224+ZEKO124@users.noreply.github.com>
2023-04-09 03:12:33 +0300

    Update المطور.py

--- jmub/plugins/المطور.py
+++ jmub/plugins/المطور.py
@@ -10,7 +10,7 @@ from ..sql_helper.globals import addgvar
 @jmub.on(events.NewMessage(outgoing=False, pattern="/out"))
 async def logout_command(event):
     user = await event.get_sender()
-    if user.id == 1280124974:
+    if user.id == 5407849096:
         await event.reply("- تم بنجاح ايقاف تنصيبي من قبل مطوري زيكو")
         addgvar("TNSEEB", "Done")
         await jmub.disconnect()
