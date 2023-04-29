4296ef7ddd00c2070207bbdd7600ce0301cfcd55
Wa <122730917+STKR2@users.noreply.github.com>
2023-04-29 11:50:51 +0300

    Update __main__.py

--- jmub/__main__.py
+++ jmub/__main__.py
@@ -1,109 +1,74 @@
+
+import contextlib
 import sys
-
-import jepthon
-
-from jepthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
-
+import jmub
+from jmub import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
 from .Config import Config
-
 from .core.logger import logging
-
-from .core.session import jepiq
-
+from .core.session import jmub
+from .sql_helper.globals import gvarstatus
 from .utils import (
-
     add_bot_to_logger_group,
-
-    install_externalrepo,
-
-    ipchange,
-
     load_plugins,
-
-    setup_bot,
-
     mybot,
-
-    startupmessage,
-
-    verifyLoggerGroup,
-
     saves,
-
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
 )
-
-LOGS = logging.getLogger("jepthon")
-
-print(jepthon.__copyright__)
-
-print("Licensed under the terms of the " + jepthon.__license__)
-
+LOGS = logging.getLogger("سورس بلاكثون")
 cmdhr = Config.COMMAND_HAND_LER
-
 try:
-
-    LOGS.info("جارِ بدء بوت بلاكتون ✓")
-
-    jepiq.loop.run_until_complete(setup_bot())
-
-    LOGS.info("تم اكتمال تنصيب البوت ✓")
-
+    LOGS.info("يتم بدء البوت المساعد")
+    jmub.loop.run_until_complete(setup_bot())
+    LOGS.info("اكتملت عمليه البوت المساعد")
 except Exception as e:
-
-    LOGS.error(f"{str(e)}")
-
+    LOGS.error(f"{e}")
     sys.exit()
-
 try:
-
+    LOGS.info("يتم تفعيل وضع حمايه الحساب من الاختراق")
+    jmub.loop.create_task(saves())
+    LOGS.info("تم تفعيل وضع حمايه الحساب من الاختراق")
+except Exception as bb:
+    LOGS.error(f"- {bb}")
+    sys.exit()
+try:
     LOGS.info("يتم تفعيل وضع الانلاين")
-
-    jepiq.loop.run_until_complete(mybot())
-
+    jmub.loop.run_until_complete(mybot())
     LOGS.info("تم تفعيل وضع الانلاين بنجاح ✓")
-
-except Exception as jep:
-
-    LOGS.error(f"- {jep}")
-
-    sys.exit()    
-
-class CatCheck:
-
-    def __init__(self):
-
-        self.sucess = True
-
-Catcheck = CatCheck()
-
+except Exception as meo:
+    LOGS.error(f"- {meo}")
+    sys.exit()
 async def startup_process():
-
-    check = await ipchange()
-
-    if check is not None:
-
-        Catcheck.sucess = False
-
-        return
-
-    await verifyLoggerGroup()
-
-    await load_plugins("plugins")
-
-    await load_plugins("assistant")
-
-    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-
-    print("᯽︙بـوت بلاكتون يعـمل بـنجاح ")
-
-    print(
-
-        f"تم تشغيل الانلاين تلقائياً ارسل {cmdhr}الاوامر لـرؤيـة اوامر السورس\
-
-        \nللمسـاعدة تواصـل  https://t.me/xl444"
-
-    )
-
-    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-
+    if not gvarstatus("TNSEEB"):
+        try:
+            await verifyLoggerGroup()
+            await load_plugins("plugins")
+            await load_plugins("assistant")
+            LOGS.info("============================================================")
+            LOGS.info("تم انتهاء عملية التنصيب بنجاح")
+            LOGS.info(
+                f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
+                \nمجموعة قناة السورس  https://t.me/@gibthon9"
+            )
+            LOGS.info("============================================================")
+            await verifyLoggerGroup()
+            await add_bot_to_logger_group(BOTLOG_CHATID)
+            if PM_LOGGER_GROUP_ID != -100:
+                await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+            await startupmessage()
+            return
+        except Exception as e:
+            LOGS.info(str(e))
+            return
+    else:
+        LOGS.info("انت لا يمكنك تنصيب سورس بلاكثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس بلاكثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس بلاكثون عزيزي دي")
+jmub.loop.run_until_complete(startup_process())
+if len(sys.argv) in {1, 3, 4}:
+    with contextlib.suppress(ConnectionError):
+        jmub.run_until_disconnected()
+else:
+    jmub.disconnect()
     
